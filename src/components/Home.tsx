659d59580bfe2e3f4fb0088a5d05d3afc60c7ce6
Herb Segismar <herbert.segismar@yahoo.com>
2025-01-21 18:03:53 +1300

    minor 2

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ type Props = {}
 
 export default function Home({}: Props) {
   return (
-    <div className="absolute w-screen h-[100vh] -z-10">
+    <div className="absolute w-screen h-[100vh] -z-10 left-0">
       <Canvas>
         <ambientLight intensity={3} />
         <Can position={[0, -1, 0]} scale={0.2} />
